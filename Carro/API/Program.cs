5b30cd0efda9b9c20be1f0dd18e2f85d4f545e6a
Rhafael Freitas Da Costa <rhafael@pf.up.edu.br>
2025-03-27 11:29:16 -0300

    Adicionado integração com banco de dados

--- Carro/API/Program.cs
+++ Carro/API/Program.cs
@@ -1,20 +1,69 @@
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
 var app = builder.Build();
 
-List<Carro> carros = [
-    new Carro() { Id = 1, Name = "Fusca" },
-    new Carro() { Id = 2, Name = "Ferrari"}
-];
+// Endpoints relacionados ao recurso de Carros
+// GET: Lista todos os carros cadastrados
+app.MapGet("/api/carros", ([FromServices] AppDataContext ctx) => {
+    if(ctx.Carros.Any()){
+        return Results.Ok(ctx.Carros.ToList());
+    }
 
-// Exemplo de requisição 
-app.MapGet("/", () => "Hello World!");
+    return Results.NotFound();
+});
 
-// GET: api/carros
-app.MapGet("/api/carros", () => {
-    return Results.Ok(carros);
+// POST: Cadastrar carro
+app.MapPost("/api/carros", ([FromBody] Carro carro,
+                            [FromServices] AppDataContext ctx) => {
+
+    ctx.Carros.Add(carro);
+    ctx.SaveChanges();
+    return Results.Created("", carro);
+});
+
+// GET: Buscar o carro pelo id 
+app.MapGet("/api/carros/{id}", ([FromRoute] int id,
+                                [FromServices] AppDataContext ctx) => {
+    Carro? carro = ctx.Carros.Find(id);
+
+    if (carro != null) {
+        return Results.Ok(carro);
+    }
+
+    return Results.NotFound();
+});
+
+// PUT: Atualiza os dados do Carro pelo Id 
+app.MapPut("/api/carros/{id}", ([FromRoute] int id,
+                                [FromBody] Carro carro,
+                                [FromServices] AppDataContext ctx) => {
+    Carro? entidade = ctx.Carros.Find(id);
+
+    if (entidade != null) {
+        entidade.Name = carro.Name;
+        ctx.Carros.Update(entidade);
+        ctx.SaveChanges();
+        return Results.Ok(entidade);
+    }
+    return Results.NotFound();
+});
+
+// DELETE: Remove um carro pelo Id 
+app.MapDelete("/api/carros/{id}", ([FromRoute] int id,
+                                   [FromServices] AppDataContext ctx) => {   
+    Carro? carro = ctx.Carros.Find(id);
+    if(carro == null) {
+        return Results.NotFound();
+    }
+
+    ctx.Carros.Remove(carro);
+    ctx.SaveChanges();
+    return Results.NoContent();
 });
 
 app.Run();
